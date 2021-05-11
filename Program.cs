8abd0d393113133e8bdbdb68e12518648eaab7d0
unknown <Lntinfotech@INFEDLBCP1387.lntinfotech.com>
2021-05-11 10:15:56 -0400

    wrote try catch and if statements

--- Program.cs
+++ Program.cs
@@ -4,9 +4,35 @@ namespace SeniorTicket
 {
     class Program
     {
+            const int SENIOR_AGE = 65;
+            const int CHILD_AGE = 12;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int ticketPrice = 14;
+            int userAge = 20;
+            bool isStudent = false; 
+
+            try {
+                if (userAge < 0) {
+                    throw new Exception();
+                }
+
+                if (userAge >= SENIOR_AGE) {
+                ticketPrice = 10;
+                } else if (userAge <= CHILD_AGE) {
+                    ticketPrice = 8;
+                }
+
+                if (isStudent) {
+                    ticketPrice = 8;
+                }
+
+                Console.WriteLine("Your ticket price is: " + ticketPrice);
+            } catch (Exception e) {
+                Console.WriteLine("Enter a correct age " + e.Message);
+            }
+
         }
     }
 }
